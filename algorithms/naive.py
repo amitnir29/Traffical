eb4b58284a3e020e805433c9049633ea057c917d
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2021-06-10 11:48:46 +0300

    Adapt NaiveAlgo to work with single junction

--- algorithms/naive.py
+++ algorithms/naive.py
@@ -2,33 +2,21 @@ from algorithms.tl_manager import TLManager
 
 
 class NaiveAlgo(TLManager):
-    def __init__(self, traffic_lights, all_junctions, light_interval=10):
-        super().__init__(traffic_lights, all_junctions)
-        self.__light_groups = self.get_lights_groups()
+    def __init__(self, junction, light_interval=10):
+        super().__init__(junction)
         self.__time_count = 0
         self.__light_interval = light_interval
-        self.__curr_group = 0
         self.init_lights()
 
     def init_lights(self):
-        for tl in self.__light_groups[0]:
-            tl.change_light(False)
-
-    def get_lights_groups(self):
-        max_lights = max(len(junc.lights) for junc in self._tl_junctions)
-        groups = list()
-        for i in range(max_lights):
-            groups.append(list())
-            for junc in self._tl_junctions:
-                if len(junc.lights) >= i - 1:
-                    groups[-1].append(junc.lights[i])
-        return groups
+        self._lights[0].change_light()
+        for tl in self._lights[1:]:
+            tl.change_light()
 
     def manage_lights(self, cars):
+        curr_group = self.__time_count % len(self._lights)
         self.__time_count += 1
+
         if self.__time_count % self.__light_interval == 0:
-            for tl in self.__light_groups[self.__curr_group]:
-                tl.change_light(False)  # to red
-            for tl in self.__light_groups[(self.__curr_group + 1) % len(self.__light_groups)]:
-                tl.change_light(True)  # to green
-            self.__curr_group = (self.__curr_group + 1) % len(self.__light_groups)
+            self._lights[curr_group].change_light(False)
+            self._lights[curr_group].change_light(True)
--- algorithms/tl_manager.py
+++ algorithms/tl_manager.py
@@ -2,10 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class TLManager(ABC):
-    def __init__(self, traffic_lights, all_junctions):
-        self._traffic_lights = traffic_lights
-        self._junctions = all_junctions
-        self._tl_junctions = self.get_tl_junctions()
+    def __init__(self, junction):
+        self._junction = junction
+        self._lights = self._junction.lights
 
     @abstractmethod
     def manage_lights(self, cars):
@@ -18,6 +17,3 @@ class TLManager(ABC):
     @abstractmethod
     def init_lights(self):
         pass
-
-    def get_tl_junctions(self):
-        return [junc for junc in self._junctions if len(junc.lights) != 0]
