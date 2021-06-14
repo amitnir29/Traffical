c3d7f1e2a79b4dd17d0b85a9752ca373aee3d03b
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2021-06-15 00:30:17 +0300

    RLQ algos

--- algorithms/algos_list.py
+++ algorithms/algos_list.py
@@ -1,3 +1,4 @@
 from algorithms.naive import NaiveAlgo
 from algorithms.most_crowded import MCAlgo
 from algorithms.most_crowded_time_limit import MCTL
+from algorithms.relative_longest_q import RLQTL, RLQRS
