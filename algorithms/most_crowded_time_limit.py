04b5eea92e9750fd3fbf9170e1b75c9ffcdb542e
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2021-06-14 23:37:12 +0300

    Adapt MCTL

--- algorithms/most_crowded_time_limit.py
+++ algorithms/most_crowded_time_limit.py
@@ -1,31 +1,48 @@
 from collections import defaultdict
 from typing import Dict
 
-from algorithms.most_crowded import MCAlgo
+from algorithms.tl_manager import TLManager
 
 
-class MCTL(MCAlgo):
-    def __init__(self, traffic_lights, all_junctions, min_green_time=5, max_red_time=60):
-        super().__init__(traffic_lights, all_junctions, min_green_time)
+class MCTL(TLManager):
+
+    def __init__(self, junction, min_green_time=5, max_red_time=60):
+        super().__init__(junction)
+        self._min_green_time = min_green_time
+        self.init_lights()
+
+        self._current_light = self._lights[0] if len(self._lights) > 0 else None
         self.__max_red_time = max_red_time
 
-    def manage_lights(self, cars):
-        for junc in self._tl_junctions:
-            green = self._get_green_tl(junc)
-            if green.light_time < self._min_green_time:
-                continue
-            # also, check if there is a light that has been red for too long
-            waiting_longest = max([tl for tl in junc.lights if tl != green], key=lambda tl: tl.light_time)
-            if waiting_longest.light_time > self.__max_red_time:
-                green.change_light(False)  # to red
-                waiting_longest.change_light(True)  # to green
-                continue
-            # for each light, get number of cars in its lanes
-            cars_count: Dict[int, int] = defaultdict(int)
-            for i, tl in enumerate(junc.lights):
-                for lane in tl.lanes:
-                    cars_count[i] += lane.cars_amount()
-            mc_lane = junc.lights[max(cars_count, key=lambda k: cars_count[k])]
-            if mc_lane != green:
+    def _map_cars_amount(self):
+        # for each light, get number of cars in its lanes
+        cars_count: Dict[int, int] = defaultdict(int)
+        for i, tl in enumerate(self._junction.lights):
+            for lane in tl.lanes:
+                cars_count[i] += lane.cars_amount()
+
+        return cars_count
+
+    def manage_lights(self):
+        if len(self._lights) == 0:
+            return
+
+        green = self._current_light
+        if green.light_time < self._min_green_time:
+            return
+
+        # also, check if there is a light that has been red for too long
+        waiting_longest = max([tl for tl in self._junction.lights if tl != green], key=lambda tl: tl.light_time)
+        if waiting_longest.light_time > self.__max_red_time:
+            green.change_light(False)  # to red
+            waiting_longest.change_light(True)  # to green
+        else:
+            cars_count = self._map_cars_amount()
+            # print(cars_count.values())
+            mc_lane = self._junction.lights[max(cars_count, key=lambda k: cars_count[k])]
+            # if mc_lane != green:
+            if not mc_lane.can_pass:
                 green.change_light(False)  # to red
                 mc_lane.change_light(True)  # to green
+
+            self._current_light = mc_lane
