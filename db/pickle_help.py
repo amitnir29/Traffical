9bae77434f70802c43dd2b588a3f899ab304bf97
YoavCarmel <carmel.yoav@gmail.com>
2021-06-21 01:20:03 +0300

    added jsonpickle functions

--- db/pickle_help.py
+++ db/pickle_help.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle, jsonpickle
 
 
 def pickle_to_file(obj, filename):
@@ -11,6 +11,15 @@ def pickle_to_file(obj, filename):
     outfile.close()
 
 
+def jsonpickle_to_file(obj, filename):
+    """
+    :param obj: object to write to file
+    :param filename: the file to write to
+    """
+    with open(filename, "w") as f:
+        f.write(jsonpickle.encode(obj))
+
+
 def pickle_from_file(filename):
     """
     :param filename: file to read from
@@ -19,4 +28,15 @@ def pickle_from_file(filename):
     infile = open(filename, 'rb')
     data = pickle.load(infile)
     infile.close()
+    return data
+
+
+def jsonpickle_from_file(filename):
+    """
+    :param filename: file to read from
+    :return: the object in file
+    """
+    with open(filename, "r") as f:
+        all_text = f.read()
+    data = jsonpickle.decode(all_text)
     return data
