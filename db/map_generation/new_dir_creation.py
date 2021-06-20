4502c6e820ee054e31520c5e4f9e537cf469e2f1
YoavCarmel <carmel.yoav@gmail.com>
2021-06-21 01:24:17 +0300

    fixed creation of new dir in generated db

--- db/map_generation/new_dir_creation.py
+++ db/map_generation/new_dir_creation.py
@@ -18,9 +18,17 @@ def __get_new_dir_name() -> str:
     """
     :return: name of the new dir
     """
-    dirs = os.listdir(DIRS_PATH)
+    try:
+        dirs = os.listdir(DIRS_PATH)
+    except FileNotFoundError as e:
+        # no "generated" dir. create it
+        os.mkdir(DIRS_PATH)
+        return "0"
+    # dir exists. check the inner dirs
     dirs_nums = [int(dir_name) for dir_name in dirs]
+    # if no inner dirs:
     if len(dirs_nums) == 0:
         return "0"
+    # return the next dir number
     new_name = max(dirs_nums) + 1
     return str(new_name)
