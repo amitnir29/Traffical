c04b7f8d651d8c9559f269387f8367804ef55e34
YoavCarmel <carmel.yoav@gmail.com>
2021-07-17 15:23:25 +0300

    main cleanup

--- main.py
+++ main.py
@@ -10,52 +10,36 @@ from server.statistics.stats_reporter import StatsReporter
 
 
 def main():
-    # create the graphics object
+    # size of window
     win_width, win_height = (800, 800)
-    gm = GraphicsManager(fps=10, width=win_width, height=win_height)
-
     # get the simulation map
     roads, traffic_lights, all_junctions = create_map(win_width, win_height, "db/databases/generated/8")
-
+    # init cars list
     cars: List = generate_cars(roads, 3, p=0.9, min_len=20, with_prints=True)
-    # cars: List = generate_cars(roads, 14, p=1, min_len=6, with_prints=True)
-
-    # while the screen is not closed, draw the current state and calculate the next state
-    # light_algos = [NaiveAlgo(junction) for junction in all_junctions]
-    # light_algos = [MCAlgo(junction) for junction in all_junctions]
-    # light_algos = [MCTL(junction) for junction in all_junctions]
-    # light_algos = [RLQTL(junction) for junction in all_junctions]
-    # light_algos = [RLQRS(junction) for junction in all_junctions]
-    light_algos = [CostBased(junction) for junction in all_junctions]
-
-    # lights_algorithm = NaiveAlgo(traffic_lights, all_junctions)
-    # lights_algorithm = MCAlgo(traffic_lights, all_junctions)
-    # lights_algorithm = MCTL(traffic_lights, all_junctions)
-
-    # # run the simulation
-    # frames_count = 0
-    # while gm.draw(roads, traffic_lights, cars):
-    #     traffic_lights, cars = next_iter(light_algos, traffic_lights, cars)
-    #     frames_count += 1
-    #     if frames_count % 10 == 0:
-    #         cars.append(generate_car(roads, min_len=3))
-
-    # run the simulation
-    frames_count = 0
-    # cars = list()
-    # cars.append(Car([roads[26], roads[28], roads[16], roads[5], roads[6], roads[15], roads[24], roads[25]] * 5, 0))
-    # cars.append(Car([roads[54], roads[55]]))
-    i = 0
-
+    # init traffic lights algorithm
+    light_algos = choose_algorithm(all_junctions)
+    # init simulation's stats reporter
     reporter = StatsReporter(cars, all_junctions)
-
+    # create the graphics manager
+    gm = GraphicsManager(fps=10, width=win_width, height=win_height)
+    # while the screen is not closed, draw the current state and calculate the next state
+    frames_counter = 0
     while gm.draw(roads, traffic_lights, cars, all_junctions):
-        i = i + 1
-        print(i)
+        frames_counter = frames_counter + 1
+        print(frames_counter)
         traffic_lights, cars = next_iter(light_algos, traffic_lights, cars)
         reporter.next_iter(cars)
-
+    # when run is over, report the stats
     reporter.report()
+
+
+def choose_algorithm(all_junctions):
+    # return [NaiveAlgo(junction) for junction in all_junctions]
+    # return [MCAlgo(junction) for junction in all_junctions]
+    # return [MCTL(junction) for junction in all_junctions]
+    # return [RLQTL(junction) for junction in all_junctions]
+    # return [RLQRS(junction) for junction in all_junctions]
+    return [CostBased(junction) for junction in all_junctions]
 
 
 if __name__ == '__main__':
