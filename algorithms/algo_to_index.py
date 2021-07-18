467ffb1269cecbfebee3a5c548e07358b2c2064e
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2021-07-18 12:31:28 +0300

    Create way to treat algorithms in numeric way

--- algorithms/algo_to_index.py
+++ algorithms/algo_to_index.py
@@ -4,20 +4,20 @@ from algorithms.naive import NaiveAlgo
 from algorithms.relative_longest_q import RLQTL, RLQRS
 
 algo_to_index = {
-    NaiveAlgo: 0,
-    MCAlgo: 1,
-    MCTL: 2,
-    RLQTL: 3,
-    RLQRS: 4,
-    CostBased: 5,
+    None: 0,
+    NaiveAlgo: 1,
+    MCAlgo: 2,
+    MCTL: 3,
+    RLQTL: 4,
+    RLQRS: 5,
+    CostBased: 6,
 }
-
 
 index_to_algo = {val: key for key, val in algo_to_index.items()}
 
 
 def algos_list_to_num(algos):
     base = max(algo_to_index.values()) + 1
-    algos_indices = [algo_to_index[algo] for algo in algos]
+    algos_indices = [algo_to_index[algo.__class__] if algo is not None else algo_to_index[algo] for algo in algos]
 
     return sum(element * base ** i for i, element in enumerate(reversed(algos_indices)))
