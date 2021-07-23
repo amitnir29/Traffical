9a631cdfc72221381b54b4c33e63493ebb226a00
YoavCarmel <carmel.yoav@gmail.com>
2021-07-23 22:29:30 +0300

    small refactoring

--- graphics/menu/screens/simulation_runner.py
+++ graphics/menu/screens/simulation_runner.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import List
 
 import pygame
@@ -7,6 +8,10 @@ from graphics.simaltion_graphics import SimulationGraphics
 from server.cars_generator import generate_cars
 from server.map_creation import create_map
 from server.server_runner import next_iter
+from server.simulation_objects.cars.i_car import ICar
+from server.simulation_objects.junctions.i_junction import IJunction
+from server.simulation_objects.roadsections.i_road_section import IRoadSection
+from server.simulation_objects.trafficlights.i_traffic_light import ITrafficLight
 from server.statistics.stats_reporter import StatsReporter
 
 
@@ -22,32 +27,43 @@ class SimulationConfiguration:
         return self.map_path, self.chosen_algo, self.cars_amount, self.path_min_len, self.is_small_map
 
 
+@dataclass
+class SimulationData:
+    roads: List[IRoadSection]
+    lights: List[ITrafficLight]
+    junctions: List[IJunction]
+    cars: List[ICar]
+    lights_algo: List
+    reporter: StatsReporter
+    with_small_map: bool
+
+
 class SimulationRunner(Screen):
     def __init__(self, screen: pygame.Surface, conf: SimulationConfiguration):
         super().__init__(screen)
+        self.data = self.__create_simulation_data(conf)
+
+    def __create_simulation_data(self, conf: SimulationConfiguration) -> SimulationData:
         # get the simulation map
-        map_path, chosen_algo, cars_amount, path_min_len, is_small_map = conf.to_tuple()
+        map_path, chosen_algo, cars_amount, path_min_len, with_small_map = conf.to_tuple()
         roads, traffic_lights, all_junctions = create_map(self.screen.get_width(), self.screen.get_height(), map_path)
-        self.roads = roads
-        self.traffic_lights = traffic_lights
-        self.junctions = all_junctions
         # init cars list
-        self.cars = generate_cars(roads, cars_amount, p=0.9, min_len=path_min_len, with_prints=False)
+        cars = generate_cars(roads, cars_amount, p=0.9, min_len=path_min_len, with_prints=False)
         # init traffic lights algorithm
-        self.lights_algo = [chosen_algo(junction) for junction in all_junctions]
+        lights_algo = [chosen_algo(junction) for junction in all_junctions]
         # init simulation's stats reporter
-        self.reporter = StatsReporter(self.cars, all_junctions)
-        self.with_small_map = is_small_map
+        reporter = StatsReporter(cars, all_junctions)
+        return SimulationData(roads, traffic_lights, all_junctions, cars, lights_algo, reporter, conf.is_small_map)
 
     def display(self) -> StatsReporter:
         gm = SimulationGraphics(self.screen, fps=10)
-        if self.with_small_map:
-            gm.set_small_map(self.roads)
+        if self.data.with_small_map:
+            gm.set_small_map(self.data.roads)
         # while the screen is not closed, draw the current state and calculate the next state
         frames_counter = 0
-        while gm.draw(self.roads, self.traffic_lights, self.cars, self.junctions):
+        while gm.draw(self.data.roads, self.data.lights, self.data.cars, self.data.junctions):
             frames_counter = frames_counter + 1
-            traffic_lights, cars = next_iter(self.lights_algo, self.traffic_lights, self.cars)
-            self.reporter.next_iter(cars)
+            traffic_lights, cars = next_iter(self.data.lights_algo, self.data.lights, self.data.cars)
+            self.data.reporter.next_iter(cars)
         # when run is over, report the stats
-        return self.reporter
+        return self.data.reporter
