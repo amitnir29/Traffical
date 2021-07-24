893adad86aa73482edcf1af89ccb53b94e84083f
amitnir29 <amit292002@gmail.com>
2021-07-24 20:11:08 +0300

    scrolling in stats screen

--- server/statistics/stats_reporter.py
+++ server/statistics/stats_reporter.py
@@ -19,6 +19,8 @@ class StatsReporter:
         self.car_num = len(cars)
         self.sum_waiting_data = {'Iterations': [], 'Total Waiting Time': []}
         self.waiting_data = {'Iteration': [], 'Waiting Cars': []}
+        self.sum_neg_acc_data = {'Iterations': [], 'Total Neg Acc Time': []}
+        self.neg_acc_data = {'Iteration': [], 'Neg Acc Cars': []}
         self.curr_iter = 0
         self.total_waiting_time = 0
         self.total_neg_acc_time = 0
@@ -40,16 +42,22 @@ class StatsReporter:
     def next_iter(self, cars):
         self.curr_iter += 1
         waiting_curr = 0
+        neg_acc_curr = 0
         for car in cars:
             if car.get_speed() < 0.0001:
                 self.total_waiting_time += 1
                 waiting_curr += 1
             if car.get_acceleration() < 0:
                 self.total_neg_acc_time += 1
+                neg_acc_curr += 1
         self.waiting_data['Iteration'] += [self.curr_iter]
         self.waiting_data['Waiting Cars'] += [waiting_curr]
         self.sum_waiting_data['Iterations'] += [self.curr_iter]
         self.sum_waiting_data['Total Waiting Time'] += [self.total_waiting_time]
+        self.neg_acc_data['Iteration'] += [self.curr_iter]
+        self.neg_acc_data['Neg Acc Cars'] += [neg_acc_curr]
+        self.sum_neg_acc_data['Iterations'] += [self.curr_iter]
+        self.sum_neg_acc_data['Total Neg Acc Time'] += [self.total_neg_acc_time]
         # if self.curr_iter % MAGIC_ITER_NUMBER == 0:
         #     self.sum_waiting_data['Iterations'] += [self.curr_iter]
         #     self.sum_waiting_data['Total Waiting Time'] += [self.total_waiting_time]
@@ -111,7 +119,7 @@ class StatsReporter:
     """
 
     def report(self):
-        # general_df = pd.DataFrame(columns=['Cars', 'Jun'])
+        # Waiting data
         sum_waiting_df = pd.DataFrame(self.sum_waiting_data)
         sum_waiting_df.reset_index(drop=True, inplace=True)
         waiting_df = pd.DataFrame(self.waiting_data)
@@ -122,9 +130,8 @@ class StatsReporter:
         plt.title('Total waiting time of the entire simulation\nin each iteration')
         temp_image_mem = BytesIO()
         plt.savefig(temp_image_mem)
-        total_image = Image.open(temp_image_mem)
-        # image.show()
-        # plt.show()
+        total_waiting_image = Image.open(temp_image_mem)
+
         plt.clf()
         plt.scatter(waiting_df['Iteration'], waiting_df['Waiting Cars'])
         plt.xlabel('Iteration')
@@ -132,11 +139,36 @@ class StatsReporter:
         plt.title('Total Waiting cars in the entire simulation\nin each iteration')
         temp_image_mem2 = BytesIO()
         plt.savefig(temp_image_mem2)
-        cars_image = Image.open(temp_image_mem2)
-        # plt.show()
+        cars_waiting_image = Image.open(temp_image_mem2)
         avg_car_waiting = waiting_df['Waiting Cars'].mean()
         median_car_waiting = waiting_df['Waiting Cars'].median()
         var_car_waiting = waiting_df['Waiting Cars'].var()
+
+        # Negative Acceleration data
+        plt.clf()
+        sum_neg_acc_df = pd.DataFrame(self.sum_neg_acc_data)
+        sum_neg_acc_df.reset_index(drop=True, inplace=True)
+        neg_acc_df = pd.DataFrame(self.neg_acc_data)
+        neg_acc_df.reset_index(drop=True, inplace=True)
+        plt.plot(sum_neg_acc_df['Iterations'], sum_neg_acc_df['Total Neg Acc Time'])
+        plt.xlabel('Iterations')
+        plt.ylabel('Total Neg Acc Time')
+        plt.title('Total negative acceleration time of the\n entire simulation in each iteration')
+        temp_image_mem3 = BytesIO()
+        plt.savefig(temp_image_mem3)
+        total_neg_acc_image = Image.open(temp_image_mem3)
+
+        plt.clf()
+        plt.scatter(neg_acc_df['Iteration'], neg_acc_df['Neg Acc Cars'])
+        plt.xlabel('Iteration')
+        plt.ylabel('Neg Acc Cars')
+        plt.title('Total negative acceleration cars in the\nentire simulation in each iteration')
+        temp_image_mem4 = BytesIO()
+        plt.savefig(temp_image_mem4)
+        cars_neg_acc_image = Image.open(temp_image_mem4)
+        avg_car_neg_acc = neg_acc_df['Neg Acc Cars'].mean()
+        median_car_neg_acc = neg_acc_df['Neg Acc Cars'].median()
+        var_car_neg_acc = neg_acc_df['Neg Acc Cars'].var()
 
         return self.total_waiting_time, self.total_neg_acc_time, avg_car_waiting, median_car_waiting, var_car_waiting, self.car_num, total_image, cars_image
         # waiting_df.to_csv('try.csv')
