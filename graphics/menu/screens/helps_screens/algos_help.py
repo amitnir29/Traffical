d67232c8efb924e81865cb787e7e5b387b430a9f
amitnir29 <amit292002@gmail.com>
2021-07-24 20:11:08 +0300

    scrolling in stats screen

--- graphics/menu/screens/helps_screens/algos_help.py
+++ graphics/menu/screens/helps_screens/algos_help.py
@@ -8,8 +8,11 @@ class AlgosHelp(Screen):
     def display(self):
         self.screen.fill(self.background)
         # write the text
-        self.write_text("This is the", self.screen.get_width() // 2, self.screen.get_height() // 4, 80)
-        self.write_text("Algos help screen", self.screen.get_width() // 2, self.screen.get_height() // 4 + 80, 80)
+        self.write_text("In this screen you", self.screen.get_width() // 2, self.screen.get_height() // 4, 60)
+        self.write_text("need to choose an", self.screen.get_width() // 2, self.screen.get_height() // 4 + 60, 60)
+        self.write_text("algorithm which will", self.screen.get_width() // 2, self.screen.get_height() // 4 + 120, 60)
+        self.write_text("determine the traffic", self.screen.get_width() // 2, self.screen.get_height() // 4 + 180, 60)
+        self.write_text("light's colors", self.screen.get_width() // 2, self.screen.get_height() // 4 + 240, 60)
         self.write_text("click to go back", self.screen.get_width() // 2, 3 * self.screen.get_height() // 4, 40)
         # Draws the surface object to the screen.
         pygame.display.update()
