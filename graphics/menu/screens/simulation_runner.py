2f92f331681b6ac09899f48f425d793d29b4b6f1
YoavCarmel <carmel.yoav@gmail.com>
2021-07-25 10:27:38 +0300

    fixed a very stupid bug

--- graphics/menu/screens/simulation_runner.py
+++ graphics/menu/screens/simulation_runner.py
@@ -140,21 +140,22 @@ class SimulationRunner(Screen):
     def run_silent(self) -> List[Tuple[str, StatsReporter]]:
         self.data: ComparisonData
         # while the screen is not closed, draw the current state and calculate the next state
-        frames_counter = 0
         init_cars = deepcopy(self.data.cars)
         reporters: List[Tuple[str, StatsReporter]] = list()
         for i, lights_algo in enumerate(self.data.lights_algos):
+            frames_counter = 0
             curr_cars = deepcopy(init_cars)
             reporter = StatsReporter(curr_cars)
             while len(curr_cars) > 0:
-                self.__draw_comparison(i, lights_algo[i].__class__.__name__, frames_counter)
+                print(len(curr_cars))
+                self.__draw_comparison(i, lights_algo[0].__class__.__name__, frames_counter)
                 frames_counter = frames_counter + 1
-                traffic_lights, cars = next_iter(lights_algo, self.data.lights, curr_cars)
-                reporter.next_iter(cars)
+                traffic_lights, curr_cars = next_iter(lights_algo, self.data.lights, curr_cars)
+                reporter.next_iter(curr_cars)
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         exit()
-            reporters.append((lights_algo[i].__class__.__name__, reporter))
+            reporters.append((lights_algo[0].__class__.__name__, reporter))
         return reporters
 
     def __draw_comparison(self, index, algo_name, frames_count):
