f6893737cbba04a21bced0b74cfc5f08ce744224
YoavCarmel <carmel.yoav@gmail.com>
2021-07-26 11:12:41 +0300

    pygame screen title is now Traffical

--- gui/screen.py
+++ gui/screen.py
@@ -4,6 +4,7 @@ import pygame
 def create_screen(width, height):
     # Start pygame
     pygame.init()
+    pygame.display.set_caption('Traffical')
     screen = pygame.display.set_mode((width, height))
     return screen
 
