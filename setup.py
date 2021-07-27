a1a810f9fa384ab1d9b54347675396f5e08a7805
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2021-07-27 11:25:26 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Traffical',
+      version='1.0',
+      description='Smart traffic lights',
+      packages=[
+          'pandas==1.0.5',
+          'pygame==2.0.1',
+          'numpy==1.17.4',
+          'matplotlib==3.2.1',
+          'scikit-learn==0.23.1'],
+      )
